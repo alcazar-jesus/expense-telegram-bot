c860e111fc7c38061b335d2187be90dad3568313
Jesús <alcazarjsanchez@gmail.com>
2025-05-17 18:58:40 +0200

    cambios en el setting.py y requirements.txt

---
+++ src/settings.py
@@ -0,0 +1,21 @@
+import yaml
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+CONF_PATH = BASE_DIR / "config" / "config.yaml"
+
+
+with open(CONF_PATH, "r", encoding="utf-8") as f:
+    cfg = yaml.safe_load(f)
+
+
+TOKEN = cfg["telegram_token"]
+DATA_PATH = BASE_DIR / cfg["data_path"]
+
+
+if __name__ == '__main__':
+
+    print(TOKEN)
+    print(BASE_DIR)
+    print(CONF_PATH)
+    print(DATA_PATH)
