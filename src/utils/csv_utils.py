55eb1e44dbb4d0f28286070a6f5f8c0e0ed4f367
Jesús <alcazarjsanchez@gmail.com>
2025-05-21 22:54:49 +0200

    Arreglado problema del encabezado

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -13,9 +13,9 @@ def save_expense(expense: Expense, path: Path = DATA_FILE_PATH) -> None:
     # Comprobamos que el path existe:
     first = not path.exists()
     path.parent.mkdir(parents=True, exist_ok=True) # si no existe lo creamos
-    with open(path, 'a', encoding='utf-8') as f:
-        writer = csv.writer(f)
-
+    with open(path, 'a', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f, delimiter=';')
+        print(first)
         if first:
             writer.writerow(["user","fecha","importe","tipo","concepto","descripcion","quien","viaje","anualizable"])
 
